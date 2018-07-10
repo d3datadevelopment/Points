c3f6c85d2ecce13fde8148640304d036d9fcc6fb
MarkusGaertner <mg@shopmodule.com>
2018-07-10 09:31:32 +0200

    Update 5.0.0.1, add Docu, PHP 7.1-7.2, add some files as Source

--- src/Modules/Core/d3_oxviewconfig_points.php
+++ src/Modules/Core/d3_oxviewconfig_points.php
@@ -1,33 +1,66 @@
-<?php //006ec
-// This Software is the property of D³ Data Development and is protected by copyright law - it is NOT Freeware.  Any unauthorized use of this software without a valid license key is a violation of the license agreement and will be prosecuted by civil and criminal law.  Inhaber: Thomas Dartsch Alle Rechte vorbehalten  @package Bonuspunkte @version 5.0.0.0 PHP71 (19.04.2018) @author  Markus Gärtner support@shopmodule.com @copyright (C) 2018, D3 Data Development @see http://www.shopmodule.com
-if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo("Site error: the ".(php_sapi_name()=='cli'?'ionCube':'<a href="http://www.ioncube.com">ionCube</a>')." PHP Loader needs to be installed. This is a widely used PHP extension for running ionCube protected PHP code, website security and malware blocking.\n\nPlease visit ".(php_sapi_name()=='cli'?'get-loader.ioncube.com':'<a href="http://get-loader.ioncube.com">get-loader.ioncube.com</a>')." for install assistance.\n\n");exit(199);
-?>
-HR+cP+mcZqQV2dHfPfSQPSWN0ldfUS2PZxc9jxEu0bsu3ln+P88a1feGu4e50GGuNOkrEHTYauWg
-QwuG6PMJzaDwLX4Jn8BuP8EXR2fA7tx4xgaexobSlYGtITEpYZUJg4GG2vlM+lZ+C1tp6O0cNxQf
-9CgOG4W8cO4zNAfg3yR3nQhJu2aWG1dDnpaKp8OkCqtW5dekDPznzGjag1H+xsGZR3dt03xIwKz3
-iuGXRqT4vvXH2eAGs1ymRCz7c8QvJPvZBaFrCD1YGuWKsQvRih8uCbf65LrUyluvHBgMyFIXUbwk
-7W8I/qnyaI7B2qR7+fN2BphjXY6RYRioPodrKFSapH8RlzNGeIFpe6WvdBUSW6uVrH6yeQBMBF13
-xfKGzKPISoP3iRiTF+smV6ObnuWjkUozvq7E8onLvju/B/o76AN2vU710X7i15ZtyLhBG5zgINCa
-iXZL/FRqkyJXvfGLFsdOdZJ1THQPaol84TtHv0tHz1nUePj9LEG9xKiSO2KxbukwASMp/m/qNxcQ
-EsiufzM15LFOtyFXkC8+dwTqK5WOMNmMDRzID8cQALrhrOv7G/cl0VNhKVXGkiH0R9SLJKxg5oXc
-Ea9Jx7JZ7nEM8XLdM9WpGsPIDKB5q1HLz3bCafYqMnk/hIG6yGzSJ7fqj4/OYVK7gcSBpFGbUmwc
-+C6kmsXylZxX0RYX/FnZ6PpfBea4qdmkjBNxJjSz/alsdJ4XSjmtO3+sW5fKbwMIvXUM/7CDx4Zu
-YOHwEAjzOuvvxS8QosjAbXhaIzaTwx44UJ/nhOe5NQTNvF4Lj8CvGDuGkjlOpd6aEj75jw1DZYDl
-eC8BzcB5AMpfzANt6xQCDBRWhK+WzkGv1KLW8YxG/rv8PRf6PjtvsY5tnmFv64ZFz0R/XgkRN6K/
-ISRz5p61shJsIT79pBOEByZMPmmExXtk4qje5X5yurc8DTvPWN36gB5FEPuQdZCEu74uMf1NM8YB
-uuRYEwebK7UFWNBBzGGp9VooHEXwU886/xswJWP0KLxy6GXT+Zi3A4sLrE6pgiIAPbguuCXyL40O
-iDya4ZQULf27zRC27efW5iaKEC9tLCVeKcLH1swLggnB2YJxHL8snfjBnMNhuAPJbVE4HAGUyKNn
-7b2Aq28hz/roS6uRiv3zSOSibgWvouPefNHmTPd8mO+pqtmn5n12JY3K9xKWwt8CESiFkfm4ZOc/
-OtG/d428ILJIlRyjHstc+F7QpBtqjJremoyBl4hn3PRLkGwUJJK+HzYBIJDn33Us7FinikOFZFh1
-EWoHo5pCVgIb4hZwbObR5uU7yRpwynJfVcvEoZXeNb4pwhY9acG3/yEa3ecR68kv09CPp/Oe+YHM
-NST62lA8021C2YxkogmvkZyriGyNLkqzfO+QQyHO8Cw3++nsUccNDBSpOHwRyJ6ibgskvx4p5twP
-Z6/vpUUMZQ5rrzsSWp2F96H9lC0YnA0kI9rXuFtIicLk/pT7QmrkfEzQV83bNMJaTUpVmy5OEPMN
-r1Sh4fd4Z1lmtkTtbY8ZLDN1a6+GCJBT81WU71thPRf4Merb5cNg1F57onuZrrS9iFl0diEfwMzj
-wjWnG5bAlzjgpU/ooquJ2GM4i2osugvJ179M33cciqMCJsZmk8GuH8tj5nJOaMbfRePwbNvQePbh
-GynhlOPa3EhAz5hj2Y6UlZMkg/QqpVitPzSDKYJdSm//IV4pZrYmD+lJBkNGkMzQn+itc3Hswu01
-pxMEy8s8PX8wNCtYeVigM1ZIJpsc+gI3E3YaNVdUAAm47CWltmefqZxOKANMqnHVVWe/ctC093J4
-k8KIGQqjhuFzT1xhmHKfCryIkoVd7CxOKP/mtcI3NHORkqZZJIgtRb0c1EHg9Caf55MpfyET8PQK
-kVYNmtY3yytnq12uz0de/T98Aum9cPRs4n8vH0Pa9SGOcWMn4VMJvlu13kbJtwAADiXye+xlp84C
-2ekER8dXwTGNV6VuzUM8FO8UB35mccr64O9Gg6GCg1AxM7PoiyyjC0wzMqXQlFS7ZDG1TME/PdEo
-1I+BB3Bg8W29J9XNjWqnwmLtyvCGbPRz9a0O0s+M7X7yqKyOhGUtXv39f5UpvsF7bY3kRzSV6Mul
-kY2+QghULm==
+<?php
+/**
+ * This Software is the property of D³ Data Development
+ * and is protected by copyright law - it is NOT Freeware.
+ * Any unauthorized use of this software without a valid license
+ * key is a violation of the license agreement and will be
+ * prosecuted by civil and criminal law.
+ * D3 Data Development
+ * Inhaber: Thomas Dartsch
+ * Alle Rechte vorbehalten
+ *
+ * @package       "Bonuspunkte"
+ * @version       3.0.1_XE4
+ * @author        Thomas Dartsch <thomas.dartsch@shopmodule.com> / Markus Gärtner <markus.gaertner@shopmodule.com>
+ * @copyright (C) 2011, D3 Data Development
+ * @see           http://www.shopmodule.com
+ *                $Rev::                                               $:
+ *                $Author::                                            $:
+ *                $Date::                                              $:
+ */
+
+namespace D3\Points\Modules\Core;
+
+use OxidEsales\Eshop\Core\Theme;
+use D3\Points\Application\Model\d3points;
+
+/**
+ * Class d3_oxviewconfig_points
+ *
+ * @package D3\Points\Modules\Application\Core
+ */
+class d3_oxviewconfig_points extends d3_oxviewconfig_points_parent
+{
+    /**
+     * Gibt die Summe der aktuellen Bonuspunkte zurück
+     *
+     * @return int
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \OxidEsales\Eshop\Core\Exception\DatabaseConnectionException
+     * @throws \OxidEsales\Eshop\Core\Exception\DatabaseErrorException
+     */
+    public function d3getPointsTotalSum()
+    {
+        $oUser = $this->getUser();
+        if (!$oUser) {
+            return 0;
+        }
+
+        /* @var $oD3UserPoints d3points */
+        $oD3UserPoints = oxNew(d3points::class);
+
+        return $oD3UserPoints->d3GetTotalSumPoints($oUser->getId());
+    }
+
+    /**
+     * @return mixed
+     * @throws \OxidEsales\Eshop\Core\Exception\SystemComponentException
+     */
+    public function d3GetParentThemeId()
+    {
+        /** @var d3_oxtheme_modcfg $oTheme */
+        $oTheme = oxNew(Theme::class);
+        return $oTheme->d3GetParentThemeId();
+    }
+
+}
